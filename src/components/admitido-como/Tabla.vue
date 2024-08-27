870f51a65d4405ddbe864b10896b9b8610512d02
Gary Alexander Campusano Paredes <Ingcampusano@Outlook.com>
2024-08-27 15:47:36 -0400

    GC - Style - Agregue el + a (Agregar AdmiticoComo)

--- src/components/admitido-como/Tabla.vue
+++ src/components/admitido-como/Tabla.vue
@@ -2,7 +2,7 @@
   <div class="container mt-5">
     <div class="d-flex justify-content-between align-items-center mb-3">
       <a href="/edu-admin/admitido-como/formulario" class="btn btn-success float-end"
-        >Agregar AdmitidoComo</a
+        >+ Agregar AdmitidoComo</a
       >
     </div>
     <div class="card shadow-sm">
